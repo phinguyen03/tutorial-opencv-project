3bb3858a0a495239f41bc309a669ea33fe5045af
phinguyen03 <92086378+phinguyen03@users.noreply.github.com>
2024-02-10 21:08:02 +0700

    Add files via upload

---
+++ util.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+
+def get_limits(color):
+    c=np.uint8([[color]]) #here insert the bgr value
+    hsvB=cv2.cvtColor(c,cv2.COLOR_BGR2HSV)
+    lowerLimit = hsvB[0][0][0] - 10, 100, 100
+    upperLimit = hsvB[0][0][0] + 10, 255, 255
+
+    lowerLimit=np.array(lowerLimit,dtype=np.uint8)
+    upperLimit=np.array(upperLimit,dtype=np.uint8)
+
+    return lowerLimit,upperLimit
+
+
